57e987a34edc5cd3d6871161f7784fa44b8a2fa5
BoredKid <cisskane@gmail.com>
2023-02-27 18:56:25 +0100

    make it work

--- background.js
+++ background.js
@@ -14,22 +14,29 @@ chrome.identity.getAuthToken({interactive: true}, function(token) {
 })
 
 
-const url = 'https://www.seloger.com/annonces/achat/';
+const url = 'https://www.seloger.com/';
 
 chrome.action.onClicked.addListener(async (tab) => {
 
   if (tab.url.startsWith(url)) {
     chrome.scripting.executeScript({
         target: { tabId: tab.id },
-        func: (token)=>{
+        func: (token, url)=>{
             console.log(document)
-            console.log(document.querySelector("#__next > div > main > div.pages__CWrap-sc-jd4nxp-1.kDiSWL > div > div.pages__CWrapMain-sc-jd4nxp-3.tiDIe > div.EstateResume__EstateResumeContainer-sc-5u2qj9-0.bmnduF > div.Summarystyled__ShowcaseSummaryContainer-sc-1u9xobv-0.kfyHrz > div > div.Summarystyled__Price-sc-1u9xobv-4.fAUZHl > div.Summarystyled__PriceWrapper-sc-1u9xobv-11.fsjhCR > div > span > span").textContent)
-            const data = {
+            const price = document.querySelector("#__next > div > main > div.pages__CWrap-sc-jd4nxp-1.kDiSWL > div > div.pages__CWrapMain-sc-jd4nxp-3.tiDIe > div.EstateResume__EstateResumeContainer-sc-5u2qj9-0.bmnduF > div.Summarystyled__ShowcaseSummaryContainer-sc-1u9xobv-0.kfyHrz > div > div.Summarystyled__Price-sc-1u9xobv-4.fAUZHl > div.Summarystyled__PriceWrapper-sc-1u9xobv-11.fsjhCR > div > span > span").textContent.replace("€","").replace(" ","");
+            const neighboorhood = document.querySelector("#__next > div > main > div.pages__CWrap-sc-jd4nxp-1.kDiSWL > div > div.pages__CWrapMain-sc-jd4nxp-3.tiDIe > div:nth-child(2) > p").textContent;
+            const surface = document.querySelector("#__next > div > main > div.pages__CWrap-sc-jd4nxp-1.kDiSWL > div > div.pages__CWrapMain-sc-jd4nxp-3.tiDIe > div.EstateResume__EstateResumeContainer-sc-5u2qj9-0.bmnduF > div.Summarystyled__ShowcaseSummaryContainer-sc-1u9xobv-0.kfyHrz > div > div.Summarystyled__TitleWrapper-sc-1u9xobv-2.hFWKOR > div.Summarystyled__TagsWrapper-sc-1u9xobv-13.hRURQu > div:nth-child(2)").textContent.replace(" m²","")
+            const annual_charge = document.querySelector("#__next > div > main > div.pages__CWrap-sc-jd4nxp-1.kDiSWL > div > div.pages__CWrapMain-sc-jd4nxp-3.tiDIe > div.Coownershipstyled__Container-sc-1fk6ihs-0.jJwgiu > div > p").textContent;
+            const data={
+              values:[[
+                url,
+                price,
+                surface,
+                neighboorhood,
+                annual_charge
+              ]]
             }
-
-          console.log("hey", token)
-          
-          fetch('https://sheets.googleapis.com/v4/spreadsheets', {
+          fetch('https://sheets.googleapis.com/v4/spreadsheets/1B9F54SWBLDZABryZX73lHOuD6vom60oXu4Xb4Q9ATHs/values/RawStudioData:append?valueInputOption=USER_ENTERED', {
               method: 'POST',
               headers: {
                   'Content-Type': 'application/json',
@@ -46,7 +53,7 @@ chrome.action.onClicked.addListener(async (tab) => {
               console.log("error");
           })
         },
-        args: [AUTH_TOKEN]
+        args: [AUTH_TOKEN, tab.url]
       });
     // FOLLOWING CODE IS FROM THE EXAMPLE OF GOOGLE
     // // Retrieve the action badge to check if the extension is 'ON' or 'OFF'
